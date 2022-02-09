d63041e4e8ec65bdc43e1baa163739293e035fb7
ishikasingh <ishikasingh95@gmail.com>
2022-02-09 14:48:13 -0800

    load_tatc.py

--- src/teach/dataset/load_tatc.py
+++ src/teach/dataset/load_tatc.py
@@ -21,9 +21,9 @@ def add_images_path(base_dir, phase):
     games = []
 
     data_dir = os.path.join(base_dir, "games_with_all_file_paths", phase)
-    images_dir = os.path.join(base_dir, "prog_check_files", phase, "PC")
-    # images_dir = os.path.join(base_dir, "images", phase)
-    new_data_dir = os.path.join(base_dir, "games_with_all_file_paths", phase)
+    # images_dir = os.path.join(base_dir, "prog_check_files", phase, "PC")
+    images_dir = os.path.join(base_dir, "images", phase)
+    new_data_dir = os.path.join(base_dir, "games_final", phase)
 
     game_files = os.listdir(data_dir)
 
@@ -35,7 +35,8 @@ def add_images_path(base_dir, phase):
 
         commander_obs = {}
         driver_obs = {}
-        targetobject = {}
+        targetobjectFrame = {}
+        targetobjectMask = {}
         pc_status = {}
         game_image_dir = os.path.join(images_dir, game_file.split(".")[0])
 
@@ -43,27 +44,32 @@ def add_images_path(base_dir, phase):
             img_f = os.path.join(game_image_dir, img_file)
             time_start = ".".join(img_file.split(".")[2:4])
             if img_file.split(".")[0] == "commander":
-                commander_obs[time_start] = img_f
+                commander_obs[time_start] = img_file
             elif img_file.split(".")[0] == "driver":
-                driver_obs[time_start] = img_f
+                driver_obs[time_start] = img_file
             elif img_file.split(".")[0] == "targetobject":
-                targetobject[time_start] = img_f
+                if img_file.split(".")[1] == "frame":
+                    targetobjectFrame[time_start] = img_file
+                if img_file.split(".")[1] == "mask":
+                    targetobjectMask[time_start] = img_file
             elif img_file.split(".")[1] == "status":
-                pc_status[time_start] = img_f
+                pc_status[time_start] = img_file
             else:  ### add other frames if needed
                 pass
         # import ipdb; ipdb.set_trace()
         for idx in range(len(interactions)):
             time_start = str(interactions[idx]["time_start"])
-            # interactions[idx].commander_obs = commander_obs[time_start]
-            # interactions[idx].driver_obs = driver_obs[time_start]
-            try:
-                if interactions[idx]["action_id"] == 500:
-                    interactions[idx]["pc_json"] = pc_status[time_start]
-            except:
-                print(f)
-            # if interactions[idx]["action_id"]>500:
-            #     interactions[idx]["targetobject"] = targetobject[time_start]
+            interactions[idx]["commander_obs"] = commander_obs[time_start]
+            interactions[idx]["driver_obs"] = driver_obs[time_start]
+            # try:
+            #     if interactions[idx]["action_id"] == 500:
+            #         interactions[idx]["pc_json"] = pc_status[time_start]
+            # except:
+            #     print(f)
+            if interactions[idx]["action_id"]>500 and interactions[idx]["success"]==1:
+                interactions[idx]["targetobjectFrame"] = targetobjectFrame[time_start]
+                interactions[idx]["targetobjectMask"] = targetobjectMask[time_start]
+
         game["tasks"][0]["episodes"][0]["interactions"] = interactions
         new_f = os.path.join(new_data_dir, os.path.basename(f))
         with open(new_f, 'w') as h:
@@ -74,6 +80,6 @@ def add_images_path(base_dir, phase):
 
 if __name__ == "__main__":
     base_data_dir = "/data/anthony/teach"
-    # "train",
-    for phase in ["valid_seen", "valid_unseen"]:
+    # 
+    for phase in ["train", "valid_seen", "valid_unseen"]:
         add_images_path(base_data_dir, phase)
